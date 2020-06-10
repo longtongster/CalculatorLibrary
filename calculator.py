1ead3d1dc8f4ca5c738729cf7ad1dfe5e74bba1d
longtongster <sacha.van.weeren@gmail.com>
2020-06-10 12:55:08 +0200

    Add function for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 12:53:31 2020
+
+@author: sacha
+"""
+
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
